1cbad559e0f7ffc3f1dc7847eccbe013473b9c8e
robsonsilv4 <robsonsilv410@gmail.com>
2020-02-24 13:49:03 -0300

    add: página inicial e categorias

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,10 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<String> categorias = [
+    'Autoajuda',
+    'Programação',
+    'Matemática',
+    'Ficção',
+    'Religião',
+  ];
+
+  int _categoriaSelecionada = 0;
+
+  @override
+  void initState() {
+    // Para tela cheia
+    SystemChrome.setEnabledSystemUIOverlays([]);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24),
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 24.0),
+                      child: Text(
+                        'Procurar',
+                        style: TextStyle(
+                          fontSize: 26,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Text(
+                    'Recomendados',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey.shade400,
+                    ),
+                  )
+                ],
+              ),
+              Container(
+                height: 80,
+                width: MediaQuery.of(context).size.width,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    itemCount: categorias.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 6.0),
+                        child: GestureDetector(
+                          onTap: () {
+                            _categoriaSelecionada = index;
+                            setState(() {});
+                          },
+                          child: Chip(
+                            padding: EdgeInsets.symmetric(horizontal: 8.0),
+                            backgroundColor: index == _categoriaSelecionada
+                                ? Colors.blue
+                                : Colors.grey.shade200,
+                            label: Text(
+                              categorias.elementAt(index),
+                              style: TextStyle(
+                                  color: index == _categoriaSelecionada
+                                      ? Colors.white
+                                      : Colors.grey.shade500),
+                            ),
+                          ),
+                        ),
+                      );
+                    }),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
